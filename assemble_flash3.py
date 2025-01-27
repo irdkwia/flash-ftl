834759b235151d2c4dda5d26b2b2ffb7adff2ff3
irdkwia <irdkwia2000@outlook.com>
2025-01-27 20:43:45 +0100

    FlashFX3.00

---
+++ assemble_flash3.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+
+parser = argparse.ArgumentParser(description="Keitai FlashFX3.00 Assemble")
+parser.add_argument("input")
+parser.add_argument("output")
+
+args = parser.parse_args()
+
+os.makedirs(args.output, exist_ok=True)
+
+MAGIC = b"\xCC\xDD\x44\x4C\x5F\x46\x53\x33\x2E\x30\x30\xFF\xFF\xFF\xFF\xFF"
+
+array = {}
+with open(args.input, "rb") as file:
+    data = file.read(0x20000)
+    while len(data) > 0:
+        if data[:0x10] == MAGIC:
+            shift = 0
+            if data[0x202:0x204] == b"\xFF\x4F":
+                shift = 9
+            elif data[0x402:0x404] == b"\xFF\xCF":
+                shift = 10
+            if shift > 0:
+                if int.from_bytes(data[0x34:0x36], "little") != 0xFFFF:
+                    region = int.from_bytes(data[0x14:0x16], "little")
+                    rst = int.from_bytes(data[0x20:0x24], "little")
+                    rlg = int.from_bytes(data[0x24:0x28], "little")
+                    array[region] = array.get(region, {})
+                    array[region][rst] = array[region].get(rst, bytearray(rlg))
+                    off = (1 << shift) + 0x4
+                    while data[off : off + 4] != b"\xFF\xFF\xFF\xFF":
+                        v = data[off + 2] + (data[off + 3] << 8)
+                        st = data[off + 1] << shift
+                        ed = data[off] << shift
+                        if v & 0x4000:
+                            ofs = (v & 0x3FFF) << shift
+                            array[region][rst][ofs : ofs + ed] = data[st : st + ed]
+                        off += 4
+        data = file.read(0x20000)
+
+for k, v in array.items():
+    with open(os.path.join(args.output, "region_%04d.bin" % k), "wb") as file:
+        buffer = bytearray()
+        for e, s in sorted(v.items()):
+            if len(buffer) < e:
+                buffer += bytes(e - len(buffer))
+            buffer[e : e + len(s)] = s
+        file.write(buffer)
