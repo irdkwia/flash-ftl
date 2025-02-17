44e128378a83a98ea084625ad55b7c3d44b582fa
irdkwia <irdkwia2000@outlook.com>
2025-02-17 19:36:15 +0100

    Support different buffer size

--- assemble_flash3.py
+++ assemble_flash3.py
@@ -13,31 +13,32 @@ MAGIC = b"\xCC\xDD\x44\x4C\x5F\x46\x53\x33\x2E\x30\x30\xFF\xFF\xFF\xFF\xFF"
 
 array = {}
 with open(args.input, "rb") as file:
-    data = file.read(0x20000)
+    data = file.read(0x200)
     while len(data) > 0:
         if data[:0x10] == MAGIC:
-            shift = 0
-            if data[0x202:0x204] == b"\xFF\x4F":
-                shift = 9
-            elif data[0x402:0x404] == b"\xFF\xCF":
-                shift = 10
-            if shift > 0:
-                if int.from_bytes(data[0x34:0x36], "little") != 0xFFFF:
+            ld = int.from_bytes(data[0x18:0x1C], "little")
+            data += file.read(ld - 0x200)
+            if int.from_bytes(data[0x34:0x36], "little") != 0xFFFF:
+                shift = int.from_bytes(data[0x36:0x38], "little")
+                if data[shift + 2 : shift + 4] in (b"\xFF\x4F", b"\xFF\xCF"):
+                    vs = int.from_bytes(data[shift : shift + 2], "little")
                     region = int.from_bytes(data[0x14:0x16], "little")
                     rst = int.from_bytes(data[0x20:0x24], "little")
                     rlg = int.from_bytes(data[0x24:0x28], "little")
                     array[region] = array.get(region, {})
                     array[region][rst] = array[region].get(rst, bytearray(rlg))
-                    off = (1 << shift) + 0x4
+                    off = shift + 0x4
                     while data[off : off + 4] != b"\xFF\xFF\xFF\xFF":
                         v = data[off + 2] + (data[off + 3] << 8)
-                        st = data[off + 1] << shift
-                        ed = data[off] << shift
+                        st = data[off + 1] * shift
+                        ed = data[off] * shift
+                        if v & 0x8000 and vs >= 3:
+                            st += 256 * shift
                         if v & 0x4000:
-                            ofs = (v & 0x3FFF) << shift
+                            ofs = (v & 0x3FFF) * shift
                             array[region][rst][ofs : ofs + ed] = data[st : st + ed]
                         off += 4
-        data = file.read(0x20000)
+        data = file.read(0x200)
 
 for k, v in array.items():
     with open(os.path.join(args.output, "region_%04d.bin" % k), "wb") as file:
