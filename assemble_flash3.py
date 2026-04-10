3132dbff464112646214b0f72954780269a5318c
irdkwia <irdkwia2000@outlook.com>
2026-04-10 11:17:54 +0200

    Simplify read

--- assemble_flash3.py
+++ assemble_flash3.py
@@ -32,11 +32,10 @@ with open(args.input, "rb") as file:
                         v = data[off + 2] + (data[off + 3] << 8)
                         st = data[off + 1] * shift
                         ed = data[off] * shift
-                        if v & 0x8000 and vs >= 3:
-                            st += 256 * shift
-                        if v & 0x4000:
-                            ofs = (v & 0x3FFF) * shift
-                            array[region][rst][ofs : ofs + ed] = data[st : st + ed]
+                        # TODO: This needs to be applied somehow
+                        # if v & 0x8000 and vs >= 3: st += 256 * shift
+                        ofs = (v & 0x3FFF) * shift
+                        array[region][rst][ofs : ofs + ed] = data[st : st + ed]
                         off += 4
         data = file.read(0x200)
 
